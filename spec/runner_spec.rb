f1667ed9c8005be8e00cd19bad3777052c22d262
Cameron Scott <camscottgreen@hotmail.com>
2019-09-03 15:44:19 +1000

    Update runner spec using RSpec each hook

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -2,13 +2,14 @@ require './lib/connect4_runner.rb'
 
 describe GameRunner do
     describe "#initialize" do
+        before(:each) do
+            @match = GameRunner.new("B","C")
+        end
         it "sets the two players" do
-            match = GameRunner.new("B","C")
-            expect(match.players).to eql(["B","C"])
+            expect(@match.players).to eql(["B","C"])
         end
         it "sets the active player to the first player" do
-            match = GameRunner.new("B","C")
-            expect(match.activeplayer).to eql("B")
+            expect(@match.activeplayer).to eql("B")
         end
     end
 end
